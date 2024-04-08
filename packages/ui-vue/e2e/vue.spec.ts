d7f2da2fb24f2d6b563b05c9a20bf90e141c2fb4
eyelidlessness <gnosis@gmail.com>
2024-04-08 12:15:45 -0700

    Demonstrate potential stopgap way to detect errors in e2e tests

--- packages/ui-vue/e2e/vue.spec.ts
+++ packages/ui-vue/e2e/vue.spec.ts
@@ -1,6 +1,11 @@
 import { faker } from '@faker-js/faker';
 import { expect, test } from '@playwright/test';
 
+declare global {
+	// eslint-disable-next-line no-var
+	var playwrightCapturedErrors: Error[] | undefined;
+}
+
 test('All forms are rendered and there is no console error', async ({ page }) => {
 
 	let consoleErrors = 0;
@@ -12,6 +17,10 @@ test('All forms are rendered and there is no console error', async ({ page }) =>
 	});
 
 	await page.goto('/');
+
+	await page.evaluate(() => {
+		globalThis.playwrightCapturedErrors = [];
+	});
 
 	const forms = await page.getByText('Show').all();	
 
@@ -51,4 +60,9 @@ test('All forms are rendered and there is no console error', async ({ page }) =>
   // Assert that there's no console errors
 	expect(consoleErrors).toBe(0);
 
+	const capturedErrors = await page.evaluate(() => {
+		return globalThis.playwrightCapturedErrors?.length;
+	});
+
+	expect(capturedErrors).toBe(0);
 });
