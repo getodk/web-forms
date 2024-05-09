8637ee8af023e09235b8503b26780790c8cc61c1
Sadiq Khoja <sadiqkhoja@gmail.com>
2024-05-09 15:53:12 -0400

    fixes #85: convert all chai-assertions to jest

--- packages/xpath/test/helpers.ts
+++ packages/xpath/test/helpers.ts
@@ -154,7 +154,7 @@ export class TestContext<XForms extends boolean = false> {
 
 		const { message } = options;
 
-		expect(booleanValue, message).to.equal(expected);
+		expect(booleanValue, message).toEqual(expected);
 	}
 
 	assertNodeSet(
@@ -164,7 +164,7 @@ export class TestContext<XForms extends boolean = false> {
 	) {
 		const nodes = this.evaluateNodeSet(expression, options.contextNode);
 
-		expect(nodes).to.deep.equal(expected);
+		expect(nodes).toEqual(expected);
 	}
 
 	assertUnorderedNodeSet(
@@ -175,7 +175,7 @@ export class TestContext<XForms extends boolean = false> {
 		const expectedSet = new Set(expected);
 		const actual = new Set(this.evaluateUnorderedNodeSet(expression, options.contextNode));
 
-		expect(actual.size).to.equal(expectedSet.size);
+		expect(actual.size).toEqual(expectedSet.size);
 		expect(expected.every((node) => actual.has(node)));
 	}
 
@@ -194,9 +194,10 @@ export class TestContext<XForms extends boolean = false> {
 		const { message } = options;
 
 		if (Number.isNaN(expected)) {
-			expect(numberValue, message).to.be.NaN;
+			expect(numberValue, message).toBeNaN();
 		} else {
-			expect(numberValue, message).to.equal(expected);
+			// numberValue can be -0; jest's toEqual fails when -0 is compared with 0
+			expect(expected === numberValue, message).toEqual(true);
 		}
 	}
 
@@ -215,7 +216,7 @@ export class TestContext<XForms extends boolean = false> {
 
 		const { message } = options;
 
-		expect(Math.round(numberValue * factor) / factor, message).to.equal(expected);
+		expect(Math.round(numberValue * factor) / factor, message).toEqual(expected);
 	}
 
 	assertStringValue(
@@ -232,7 +233,7 @@ export class TestContext<XForms extends boolean = false> {
 
 		const { message } = options;
 
-		expect(stringValue, message).to.equal(expected);
+		expect(stringValue, message).toEqual(expected);
 	}
 
 	assertStringLength(
@@ -247,7 +248,7 @@ export class TestContext<XForms extends boolean = false> {
 			options.namespaceResolver
 		);
 
-		expect(stringValue.length).to.equal(expected);
+		expect(stringValue.length).toEqual(expected);
 	}
 
 	assertStringMatches(
@@ -264,7 +265,7 @@ export class TestContext<XForms extends boolean = false> {
 
 		const { message } = options;
 
-		expect(stringValue, message).to.match(pattern);
+		expect(stringValue, message).toMatch(pattern);
 	}
 
 	assertThrows(): never {
