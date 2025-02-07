44655b6bab1a9213af637d77742a2945eb225f09
latin-panda <66472237+latin-panda@users.noreply.github.com>
2025-02-06 22:07:12 -0600

    Adds nodeOptions to access attributes

--- packages/xforms-engine/src/client/InputNode.ts
+++ packages/xforms-engine/src/client/InputNode.ts
@@ -31,6 +31,33 @@ export interface InputDefinition<V extends ValueType = ValueType> extends LeafNo
 
 export type InputNodeAppearances = NodeAppearances<InputDefinition>;
 
+interface StringInputNodeOptions {
+	readonly rows: number | null;
+}
+
+interface GeoInputNodeOptions {
+	readonly accuracyThreshold: number | null;
+	readonly unacceptableAccuracyThreshold: number | null;
+}
+
+interface InputNodeOptionsByValueType {
+	readonly string: StringInputNodeOptions;
+	readonly int: null;
+	readonly boolean: null;
+	readonly decimal: null;
+	readonly date: null;
+	readonly time: null;
+	readonly dateTime: null;
+	readonly geopoint: GeoInputNodeOptions;
+	readonly geotrace: GeoInputNodeOptions;
+	readonly geoshape: GeoInputNodeOptions;
+	readonly binary: null;
+	readonly barcode: null;
+	readonly intent: null;
+}
+
+export type InputNodeOptions<V extends ValueType> = InputNodeOptionsByValueType[V];
+
 /**
  * A node corresponding to form field defined as an
  * {@link https://getodk.github.io/xforms-spec/#body-elements | XForms `<input>`},
@@ -41,6 +68,7 @@ export interface InputNode<V extends ValueType = ValueType>
 	readonly nodeType: 'input';
 	readonly valueType: V;
 	readonly appearances: InputNodeAppearances;
+	readonly nodeOptions: InputNodeOptions<V>;
 	readonly definition: InputDefinition<V>;
 	readonly root: RootNode;
 	readonly parent: GeneralParentNode;
--- packages/xforms-engine/src/parse/body/control/InputControlDefinition.ts
+++ packages/xforms-engine/src/parse/body/control/InputControlDefinition.ts
@@ -11,10 +11,16 @@ export class InputControlDefinition extends ControlDefinition<'input'> {
 
 	readonly type = 'input';
 	readonly appearances: InputAppearanceDefinition;
+	readonly rows: number | null;
+	readonly accuracyThreshold: number | null;
+	readonly unacceptableAccuracyThreshold: number | null;
 
 	constructor(form: XFormDefinition, parent: BodyElementParentContext, element: Element) {
 		super(form, parent, element);
 
 		this.appearances = inputAppearanceParser.parseFrom(element, 'appearance');
+		this.rows = Number(element.getAttribute('rows')); // ToDo: Make parser function, validate NaN
+		this.accuracyThreshold = Number(element.getAttribute('accuracyThreshold')); // ToDo: Make parser function, validate NaN
+		this.unacceptableAccuracyThreshold = Number(element.getAttribute('unacceptableAccuracyThreshold')); // ToDo: Make parser function, validate NaN
 	}
 }
