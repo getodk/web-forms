77d4a25febf8e59dbd70b4527d45e6667e8fa762
latin-panda <66472237+latin-panda@users.noreply.github.com>
2025-08-12 19:20:25 +0700

    add focus to input after refreshing view

--- packages/web-forms/src/components/form-elements/input/InputNumbersAppearance.vue
+++ packages/web-forms/src/components/form-elements/input/InputNumbersAppearance.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import InputText from 'primevue/inputtext';
 import type { StringInputNode } from '@getodk/xforms-engine';
-import { computed, inject, ref } from 'vue';
+import { type ComponentPublicInstance, computed, inject, nextTick, ref, watch } from 'vue';
 
 interface InputNumbersAppearanceProps {
 	readonly node: StringInputNode;
@@ -9,6 +9,7 @@ interface InputNumbersAppearanceProps {
 
 const props = defineProps<InputNumbersAppearanceProps>();
 
+const inputRef = ref<ComponentPublicInstance | null>(null);
 const doneAnswering = inject<boolean>('doneAnswering');
 const submitPressed = inject<boolean>('submitPressed');
 const invalid = computed(() => props.node.validationState.violation?.valid === false);
@@ -19,18 +20,26 @@ const inputValue = computed({
 		return props.node.currentState.value;
 	},
 	set(value) {
-		const filteredValue = value.replace(/[^0-9,\.\-]/g, '');
+		// Remove characters that are not digits, commas, periods, or minus signs
+		const filteredValue = value.replace(/[^0-9,.-]/g, '');
 		props.node.setValue(filteredValue);
-		// Increment renderKey to refresh UI with filtered value.
-		renderKey.value++;
+
+		if (value !== filteredValue) {
+			// Re-render to display cleaned value
+			renderKey.value++;
+		}
 	},
 });
+
+// After re-render, refocus input so user can continue typing seamlessly
+watch(renderKey, async () => nextTick(() => (inputRef.value?.$el as HTMLElement)?.focus()));
 </script>
 
 <template>
 	<InputText
 		:id="node.nodeId"
 		:key="renderKey"
+		ref="inputRef"
 		v-model="inputValue"
 		:required="node.currentState.required"
 		:disabled="node.currentState.readonly"
