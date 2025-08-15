2b5b3476191976c7ba92a101e1c5bb6b3db63d3c
latin-panda <66472237+latin-panda@users.noreply.github.com>
2025-08-15 21:32:11 +0700

    adds support for thousands-sep appearance in string type with numbers appearance

--- packages/web-forms/e2e/test-cases/all-question-types.test.ts
+++ packages/web-forms/e2e/test-cases/all-question-types.test.ts
@@ -33,15 +33,17 @@ test.describe('All Question Types', () => {
 
 	test.describe('Text Question Types', () => {
 		test.describe('String type', () => {
+			const questionLabel = 'String';
+
 			test('allows all characters and long values', async () => {
-				await formPage.input.fillAndExpectInputValue('String', '-123.098,56', '-123.098,56');
+				await formPage.input.fillAndExpectInputValue(questionLabel, '-123.098,56', '-123.098,56');
 				await formPage.input.fillAndExpectInputValue(
-					'String',
+					questionLabel,
 					'123456789012345678901234567890',
 					'123456789012345678901234567890'
 				);
 				await formPage.input.fillAndExpectInputValue(
-					'String',
+					questionLabel,
 					'abcdefghijklmnopqrstñňáãûüêè`(&%$#&%$##"#明日でも',
 					'abcdefghijklmnopqrstñňáãûüêè`(&%$#&%$##"#明日でも'
 				);
@@ -49,33 +51,57 @@ test.describe('All Question Types', () => {
 		});
 
 		test.describe('String type with "numbers" appearance', () => {
+			const questionLabel = 'String number';
+
 			test('disallows invalid characters', async () => {
-				await formPage.input.fillAndExpectInputValue('String number', 'abce', '');
-				await formPage.input.fillAndExpectInputValue('String number', '$#!Sds)=;+(', '');
-				await formPage.input.fillAndExpectInputValue(
-					'String number',
-					'233-abc-456-tgh',
-					'233--456-'
-				);
-				await formPage.input.fillAndExpectInputValue('String number', '765.abc', '765.');
+				await formPage.input.fillAndExpectInputValue(questionLabel, 'abce', '');
+				await formPage.input.fillAndExpectInputValue(questionLabel, '$#!Sds)=;+(', '');
+				await formPage.input.fillAndExpectInputValue(questionLabel, '233-abc-456-tgh', '233--456-');
+				await formPage.input.fillAndExpectInputValue(questionLabel, '765.abc', '765.');
 			});
 
 			test('allows digits, comma, dot, minus sign and long values', async () => {
-				await formPage.input.fillAndExpectInputValue('String number', '-123.098,56', '-123.098,56');
+				await formPage.input.fillAndExpectInputValue(questionLabel, '-123.098,56', '-123.098,56');
 				await formPage.input.fillAndExpectInputValue(
-					'String number',
+					questionLabel,
 					'123456789012345678901234567890',
 					'123456789012345678901234567890'
 				);
 				await formPage.input.fillAndExpectInputValue(
-					'String number',
+					questionLabel,
 					'233-999-456-432',
 					'233-999-456-432'
 				);
 				await formPage.input.fillAndExpectInputValue(
-					'String number',
+					questionLabel,
 					'765.235,32.742,22',
 					'765.235,32.742,22'
+				);
+			});
+		});
+
+		test.describe('String type with "numbers" and "thousands-sep" appearances', () => {
+			const questionLabel = 'String number with thousands separator';
+
+			test('disallows invalid characters', async () => {
+				await formPage.input.fillAndExpectInputValue(questionLabel, 'abce', '');
+				await formPage.input.fillAndExpectInputValue(questionLabel, '$#!Sds)=;+(', '');
+				await formPage.input.fillAndExpectInputValue(questionLabel, '233-abc-456-tgh', '233,456');
+				// Last digit dot to allow user type decimals
+				await formPage.input.fillAndExpectInputValue(questionLabel, '765.', '765.');
+				await formPage.input.fillAndExpectInputValue(
+					questionLabel,
+					'765.235,32.742,22',
+					'765.2353274222'
+				);
+			});
+
+			test('allows digits, comma, dot, minus sign and long values', async () => {
+				await formPage.input.fillAndExpectInputValue(questionLabel, '-123098.56', '-123,098.56');
+				await formPage.input.fillAndExpectInputValue(
+					questionLabel,
+					'5123456789012345678901234567.890',
+					'5,123,456,789,012,345,678,901,234,567.890'
 				);
 			});
 		});
@@ -83,49 +109,53 @@ test.describe('All Question Types', () => {
 
 	test.describe('Numerical question types', () => {
 		test.describe('Integer type', () => {
+			const questionLabel = 'Integer';
+
 			test('allows up to 9 digits', async () => {
-				await formPage.input.typeAndExpectInputValue('Integer', '1234567891234', '123456789');
-				await formPage.input.typeAndExpectInputValue('Integer', '-12312312345', '-12312312');
-				await formPage.input.typeAndExpectInputValue('Integer', '678', '678');
+				await formPage.input.typeAndExpectInputValue(questionLabel, '1234567891234', '123456789');
+				await formPage.input.typeAndExpectInputValue(questionLabel, '-12312312345', '-12312312');
+				await formPage.input.typeAndExpectInputValue(questionLabel, '678', '678');
 			});
 
 			test('disallows decimals and other characters that are not minus sign and digits', async () => {
-				await formPage.input.typeAndExpectInputValue('Integer', '-123.098,56', '-12309856');
-				await formPage.input.typeAndExpectInputValue('Integer', 'avbt234%$"3.,8', '23438');
+				await formPage.input.typeAndExpectInputValue(questionLabel, '-123.098,56', '-12309856');
+				await formPage.input.typeAndExpectInputValue(questionLabel, 'avbt234%$"3.,8', '23438');
 			});
 
 			test('Rounds up using the "round half up" rule when a decimal value is pasted into the input', async () => {
 				await formPage.copyToClipboard('146.89');
-				await formPage.input.pasteAndExpectInputValue('Integer', '147');
+				await formPage.input.pasteAndExpectInputValue(questionLabel, '147');
 
 				await formPage.copyToClipboard('146.45');
-				await formPage.input.pasteAndExpectInputValue('Integer', '146');
+				await formPage.input.pasteAndExpectInputValue(questionLabel, '146');
 			});
 		});
 
 		test.describe('Decimal type', () => {
+			const questionLabel = 'Decimal';
+
 			test('allows up to 15 digits and decimals', async () => {
 				await formPage.input.typeAndExpectInputValue(
-					'Decimal',
+					questionLabel,
 					'1234567891234567890',
 					'123456789123456'
 				);
 				await formPage.input.typeAndExpectInputValue(
-					'Decimal',
+					questionLabel,
 					'-1231231234567890',
 					'-12312312345678'
 				);
 				await formPage.input.typeAndExpectInputValue(
-					'Decimal',
+					questionLabel,
 					'-12312.31234567890',
 					'-12312.31234567'
 				);
-				await formPage.input.typeAndExpectInputValue('Decimal', '678.765', '678.765');
+				await formPage.input.typeAndExpectInputValue(questionLabel, '678.765', '678.765');
 			});
 
 			test('disallows other characters that are not minus sign and digits', async () => {
-				await formPage.input.typeAndExpectInputValue('Decimal', '-123.098,56', '-123.09856');
-				await formPage.input.typeAndExpectInputValue('Decimal', 'avbt234%$"3.,8', '2343.8');
+				await formPage.input.typeAndExpectInputValue(questionLabel, '-123.098,56', '-123.09856');
+				await formPage.input.typeAndExpectInputValue(questionLabel, 'avbt234%$"3.,8', '2343.8');
 			});
 		});
 	});
