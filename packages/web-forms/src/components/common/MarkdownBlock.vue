7ccc1a6f8638ce16ef7f18d29e424371e01537b7
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2025-09-30 09:06:08 +1300

    day 2 poc

---
+++ packages/web-forms/src/components/common/MarkdownBlock.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import MarkdownBlock from '@/components/common/MarkdownBlock.vue';
+
+interface MarkdownProps {
+	readonly elem: any; // export MarkdownElement from client interface
+}
+
+const { elem } = defineProps<MarkdownProps>();
+</script>
+
+<template>
+	<strong v-if="elem.elementName === 'strong'">
+		<MarkdownBlock
+			v-for="child in elem.children"
+			:elem="child"
+		/>
+	</strong>
+	<em v-else-if="elem.elementName === 'em'">
+		<MarkdownBlock
+			v-for="child in elem.children"
+			:elem="child"
+		/>
+	</em>
+	<template v-else-if="elem.value">
+		{{elem.value}}
+	</template>
+	<MarkdownBlock
+		v-else
+		v-for="child in elem.children"
+		:elem="child"
+	/>
+
+</template>
