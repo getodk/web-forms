4ec6fbc0843197ae1bab45d465644cb0186d4ad4
latin-panda <66472237+latin-panda@users.noreply.github.com>
2025-09-30 22:50:08 +0700

    Feedback for line's hit tolerance

--- packages/web-forms/src/components/common/map/map-styles.ts
+++ packages/web-forms/src/components/common/map/map-styles.ts
@@ -1,21 +1,18 @@
 import type { Rule } from 'ol/style/flat';
 import mapLocationIcon from '@/assets/images/map-location.svg';
 
+const DEFAULT_STROKE_COLOR = '#3E9FCC';
+
 const DEFAULT_POINT_STYLE = {
 	'icon-src': mapLocationIcon,
 	'icon-width': 40,
 	'icon-height': 40,
 };
 
-const DEFAULT_LINE_STYLE = {
-	'stroke-width': 6,
-	'stroke-color': '#3E9FCC',
-};
-
-const DEFAULT_POLYGON_STYLE = {
-	'fill-color': 'rgba(233, 248, 255, 0.8)',
+const DEFAULT_FEATURE_STYLE = {
 	'stroke-width': 4,
-	'stroke-color': '#3E9FCC',
+	'stroke-color': DEFAULT_STROKE_COLOR,
+	'fill-color': 'rgba(233, 248, 255, 0.8)',
 };
 
 const SCALE_POINT_STYLE = {
@@ -24,47 +21,48 @@ const SCALE_POINT_STYLE = {
 	'icon-height': 50,
 };
 
-const SCALE_LINE_STYLE = {
-	'stroke-width': 8,
-	'stroke-color': '#3E9FCC',
-};
-
-const SCALE_POLYGON_STYLE = {
+const SCALE_FEATURE_STYLE = {
 	'stroke-width': 6,
 };
 
+const OUTLINE_STROKE_WIDTH = 20;
+
+const BLUE_GLOW_COLOR = 'rgba(148, 224, 237, 0.7)';
+
 const BLUE_GLOW_POINT_STYLE = {
 	'circle-radius': 30,
-	'circle-fill-color': 'rgba(148, 224, 237, 0.7)',
+	'circle-fill-color': BLUE_GLOW_COLOR,
 	'circle-displacement': [0, 0],
 };
 
-const BLUE_GLOW_LINE_STYLE = {
-	'stroke-width': 20,
-	'stroke-color': 'rgba(148, 224, 237, 0.7)',
-};
-
-const BLUE_GLOW_POLYGON_STYLE = {
-	'stroke-width': 20,
-	'stroke-color': 'rgba(148, 224, 237, 0.7)',
+const BLUE_GLOW_FEATURE_STYLE = {
+	'stroke-width': OUTLINE_STROKE_WIDTH,
+	'stroke-color': BLUE_GLOW_COLOR,
 	'fill-color': 'transparent',
 };
+
+const GREEN_GLOW_COLOR = 'rgba(34, 197, 94, 0.6)';
 
 const GREEN_GLOW_POINT_STYLE = {
 	'circle-radius': 30,
-	'circle-fill-color': 'rgba(34, 197, 94, 0.6)',
+	'circle-fill-color': GREEN_GLOW_COLOR,
 	'circle-displacement': [0, 0],
 };
 
-const GREEN_GLOW_LINE_STYLE = {
-	'stroke-width': 20,
-	'stroke-color': 'rgba(34, 197, 94, 0.6)',
+const GREEN_GLOW_FEATURE_STYLE = {
+	'stroke-width': OUTLINE_STROKE_WIDTH,
+	'stroke-color': GREEN_GLOW_COLOR,
+	'fill-color': 'transparent',
 };
 
-const GREEN_GLOW_POLYGON_STYLE = {
-	'stroke-width': 20,
-	'stroke-color': 'rgba(34, 197, 94, 0.6)',
-	'fill-color': 'transparent',
+// Increases the clickable area of the Line feature.
+const LINE_HIT_TOLERANCE = {
+	'stroke-width': OUTLINE_STROKE_WIDTH,
+	'stroke-color': 'rgba( 255, 255, 255, 0.1)',
+};
+
+const matchGeometryType = (...types: string[]) => {
+	return ['all', ['in', ['geometry-type'], ['literal', types]]];
 };
 
 export function getUnselectedStyles(
@@ -72,27 +70,23 @@ export function getUnselectedStyles(
 	selectedPropName: string,
 	savedPropName: string
 ): Rule[] {
-	const makeFilter = (type: string) => {
-		return [
-			'all',
-			['match', ['geometry-type'], type, true, false],
-			['!=', ['get', featureIdProp], ['var', selectedPropName]],
-			['!=', ['get', featureIdProp], ['var', savedPropName]],
-		];
-	};
+	const filters = [
+		['!=', ['get', featureIdProp], ['var', selectedPropName]],
+		['!=', ['get', featureIdProp], ['var', savedPropName]],
+	];
 
 	return [
 		{
-			filter: makeFilter('Point'),
+			filter: [...matchGeometryType('Point'), ...filters],
 			style: DEFAULT_POINT_STYLE,
 		},
 		{
-			filter: makeFilter('LineString'),
-			style: DEFAULT_LINE_STYLE,
+			filter: [...matchGeometryType('LineString', 'Polygon'), ...filters],
+			style: DEFAULT_FEATURE_STYLE,
 		},
 		{
-			filter: makeFilter('Polygon'),
-			style: DEFAULT_POLYGON_STYLE,
+			filter: [...matchGeometryType('LineString'), ...filters],
+			style: LINE_HIT_TOLERANCE,
 		},
 	];
 }
@@ -102,52 +96,34 @@ export function getSelectedStyles(
 	selectedPropName: string,
 	savedPropName: string
 ): Rule[] {
-	const makeFilter = (type: string) => {
-		return [
-			'all',
-			['match', ['geometry-type'], type, true, false],
-			['==', ['get', featureIdProp], ['var', selectedPropName]],
-			['!=', ['get', featureIdProp], ['var', savedPropName]],
-		];
-	};
+	const filters = [
+		['==', ['get', featureIdProp], ['var', selectedPropName]],
+		['!=', ['get', featureIdProp], ['var', savedPropName]],
+	];
 
 	return [
 		{
-			filter: makeFilter('Point'),
+			filter: [...matchGeometryType('Point'), ...filters],
 			style: [BLUE_GLOW_POINT_STYLE, DEFAULT_POINT_STYLE, SCALE_POINT_STYLE],
 		},
 		{
-			filter: makeFilter('LineString'),
-			style: [BLUE_GLOW_LINE_STYLE, DEFAULT_LINE_STYLE, SCALE_LINE_STYLE],
-		},
-		{
-			filter: makeFilter('Polygon'),
-			style: [BLUE_GLOW_POLYGON_STYLE, DEFAULT_POLYGON_STYLE, SCALE_POLYGON_STYLE],
+			filter: [...matchGeometryType('LineString', 'Polygon'), ...filters],
+			style: [BLUE_GLOW_FEATURE_STYLE, DEFAULT_FEATURE_STYLE, SCALE_FEATURE_STYLE],
 		},
 	];
 }
 
 export function getSavedStyles(featureIdProp: string, savedPropName: string): Rule[] {
-	const makeFilter = (type: string) => {
-		return [
-			'all',
-			['match', ['geometry-type'], type, true, false],
-			['==', ['get', featureIdProp], ['var', savedPropName]],
-		];
-	};
+	const filter = ['==', ['get', featureIdProp], ['var', savedPropName]];
 
 	return [
 		{
-			filter: makeFilter('Point'),
+			filter: [...matchGeometryType('Point'), filter],
 			style: [GREEN_GLOW_POINT_STYLE, DEFAULT_POINT_STYLE, SCALE_POINT_STYLE],
 		},
 		{
-			filter: makeFilter('LineString'),
-			style: [GREEN_GLOW_LINE_STYLE, DEFAULT_LINE_STYLE, SCALE_LINE_STYLE],
-		},
-		{
-			filter: makeFilter('Polygon'),
-			style: [GREEN_GLOW_POLYGON_STYLE, DEFAULT_POLYGON_STYLE, SCALE_POLYGON_STYLE],
+			filter: [...matchGeometryType('LineString', 'Polygon'), filter],
+			style: [GREEN_GLOW_FEATURE_STYLE, DEFAULT_FEATURE_STYLE, SCALE_FEATURE_STYLE],
 		},
 	];
 }
