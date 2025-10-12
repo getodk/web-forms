7c80160bee1d95897a24c00a0e9f53f02e30afd4
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2025-10-13 12:31:34 +1300

    review feedback

--- packages/web-forms/src/components/form-elements/ControlLabel.vue
+++ packages/web-forms/src/components/form-elements/ControlLabel.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
+import ImageBlock from '@/components/common/ImageBlock.vue';
 import MarkdownBlock from '@/components/common/MarkdownBlock.vue';
 import type { AnyControlNode as QuestionNode } from '@getodk/xforms-engine';
-const props = defineProps<{ question: QuestionNode }>();
+import { computed } from 'vue';
+
+const { question } = defineProps<{ question: QuestionNode }>();
+
+const text = computed(() => question.currentState.label?.formatted);
+const alt = computed(() => question.currentState.label?.asString ?? '');
+const image = computed(() => question.currentState.label?.imageSource);
+const video = computed(() => question.currentState.label?.videoSource);
+const audio = computed(() => question.currentState.label?.audioSource);
 </script>
 
 <template>
 	<label :for="question.nodeId" :class="{ required: question.currentState.required }">
-		<MarkdownBlock v-for="(elem, index) in props.question.currentState.label?.formatted" :key="index" :elem="elem" />
+		<MarkdownBlock v-for="(elem, index) in text" :key="index" :elem="elem" />
+		<div v-if="image || video || audio" class="media-content">
+			<ImageBlock v-if="image" :resource-url="image" :alt="alt" />
+
+			<!-- TODO: Implement VideoBlock component -->
+			<span v-else-if="video">🚧 Video media type is not supported</span>
+
+			<!-- TODO: Implement AudioBlock component -->
+			<span v-else-if="audio">🚧 Video media type is not supported</span>
+		</div>
 	</label>
 </template>
 
