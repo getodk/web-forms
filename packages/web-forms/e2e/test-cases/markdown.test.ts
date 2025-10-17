1c70b09cff1eb611dd08390e3c53afd69a0572eb
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2025-10-17 14:51:47 +1300

    format

--- packages/web-forms/e2e/test-cases/markdown.test.ts
+++ packages/web-forms/e2e/test-cases/markdown.test.ts
@@ -3,25 +3,24 @@ import { FillFormPage } from '../page-objects/pages/FillFormPage.ts';
 import { PreviewPage } from '../page-objects/pages/PreviewPage.ts';
 
 async function selectMultiDropdownOption(page: Page, container: Locator, ...labels: string[]) {
-	const dropdown = await container.locator('.multi-select-dropdown');
+	const dropdown = container.locator('.multi-select-dropdown');
 	await dropdown.click();
 	for (const label of labels) {
 		await page.locator(`.p-multiselect-option:has-text("${label}")`).click();
 	}
-};
+}
 
 async function selectDropdownOption(page: Page, container: Locator, label: string) {
-	const dropdown = await container.locator('.dropdown');
+	const dropdown = container.locator('.dropdown');
 	await dropdown.click();
 	await page.locator(`.p-select-option:has-text("${label}")`).click();
-};
+}
 
 /**
  * Tests the UI functionality of all question types in the "All Question Types" form,
  * including rendering, user input, and validation.
  */
 test.describe('Markdown formatting', () => {
-
 	let formPage: FillFormPage;
 
 	test.beforeEach(async ({ page }) => {
@@ -33,26 +32,30 @@ test.describe('Markdown formatting', () => {
 	});
 
 	test('renders markdown styling', async ({ page }) => {
-
-		// output
-		await formPage.input.fillAndExpectInputValue("What's your name?", '<span style="color:green">marty mcfly</span>', '<span style="color:green">marty mcfly</span>');
-		const output = await page.locator('.question-container:has-text("You said:")');
+		await formPage.input.fillAndExpectInputValue(
+			"What's your name?",
+			'<span style="color:green">marty mcfly</span>',
+			'<span style="color:green">marty mcfly</span>'
+		);
+		const output = page.locator('.question-container:has-text("You said:")');
 		await expect(output).toHaveScreenshot('user-entered.png');
 
-		const select1 = await page.locator('.question-container:has-text("Select options full")');
+		const select1 = page.locator('.question-container:has-text("Select options full")');
 		await expect(select1).toHaveScreenshot('select-options.png');
 
-		const dropdown1Container = await page.locator('.question-container:has-text("Select options minimal")');
-		selectDropdownOption(page, dropdown1Container, 'yes');
+		const dropdown1Container = page.locator(
+			'.question-container:has-text("Select options minimal")'
+		);
+		await selectDropdownOption(page, dropdown1Container, 'yes');
 		await expect(dropdown1Container).toHaveScreenshot('dropdown-options.png');
 
-		const dropdownNContainer = await page.locator('.question-container:has-text("Select multiple minimal")');
-		selectMultiDropdownOption(page, dropdownNContainer, 'yes', 'no');
+		const dropdownNContainer = page.locator(
+			'.question-container:has-text("Select multiple minimal")'
+		);
+		await selectMultiDropdownOption(page, dropdownNContainer, 'yes', 'no');
 		await expect(dropdownNContainer).toHaveScreenshot('dropdown-multiple-select.png');
 
-		const note = await page.locator('.question-container:has-text("heading 1")');
+		const note = page.locator('.question-container:has-text("heading 1")');
 		await expect(note).toHaveScreenshot('note.png');
-		
 	});
-
 });
