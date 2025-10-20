001d296bb65a513d7c3643bf8437259ad59b339e
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2025-10-21 12:15:48 +1300

    try a different approach - use full page screenshots

--- packages/web-forms/e2e/test-cases/markdown.test.ts
+++ packages/web-forms/e2e/test-cases/markdown.test.ts
@@ -19,45 +19,17 @@ test.describe('Markdown formatting', () => {
 			'<span style="color:green">marty mcfly</span>',
 			'<span style="color:green">marty mcfly</span>'
 		);
-		await formPage.text.expectScreenshot(
-			'.question-container:has-text("You said:")',
-			'user-entered.png',
-			{
-				maxDiffPixelRatio: 0.02,
-			}
-		);
-
-		await formPage.text.expectScreenshot(
-			'.question-container:has-text("Select options full")',
-			'select-options.png',
-			{
-				maxDiffPixelRatio: 0.02,
-			}
-		);
 
 		const dropdown1Container = page.locator(
 			'.question-container:has-text("Select options minimal")'
 		);
 		await formPage.select.selectDropdownOption(dropdown1Container, 'yes');
-		await expect(dropdown1Container).toHaveScreenshot('dropdown-options.png', {
-			maxDiffPixelRatio: 0.02,
-		});
 
 		const dropdownNContainer = page.locator(
 			'.question-container:has-text("Select multiple minimal")'
 		);
 		await formPage.select.selectMultiDropdownOption(dropdownNContainer, 'yes', 'no');
 
-		await expect(dropdownNContainer).toHaveScreenshot('dropdown-multiple-select.png', {
-			maxDiffPixelRatio: 0.02,
-		});
-
-		await formPage.text.expectScreenshot(
-			'.question-container:has-text("heading 1") .note-control',
-			'note.png',
-			{
-				maxDiffPixelRatio: 0.05,
-			}
-		);
+		await expect(page).toHaveScreenshot('full-page.png', { fullPage: true });
 	});
 });
