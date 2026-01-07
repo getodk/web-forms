e520893edf4db929f6cd883164ae948975909fdd
latin-panda <66472237+latin-panda@users.noreply.github.com>
2026-01-07 22:06:52 +0700

    Adds media base component

--- packages/web-forms/src/components/common/media/ImageBlock.vue
+++ packages/web-forms/src/components/common/media/ImageBlock.vue
@@ -1,38 +1,26 @@
 <script setup lang="ts">
-import { useMediaLoader } from '@/components/common/media/useMediaLoader.ts';
+import MediaBlockBase from '@/components/common/media/MediaBlockBase.vue';
 import type { JRResourceURL } from '@getodk/common/jr-resources/JRResourceURL.ts';
 import { type ObjectURL } from '@getodk/common/lib/web-compat/url.ts';
-import { computed, ref, triggerRef, watchEffect } from 'vue';
+import { computed, ref, triggerRef } from 'vue';
 
-interface ImageBlockProps {
+defineProps<{
 	readonly resourceUrl?: JRResourceURL;
 	readonly blobUrl?: ObjectURL;
 	readonly alt: string;
-}
+}>();
 
 interface NaturalDimensions {
 	readonly naturalWidth: number;
 	readonly naturalHeight: number;
 }
+const loadedDimensions = ref<NaturalDimensions>({ naturalWidth: 0, naturalHeight: 0 });
 
 const SMALL_IMAGE_SIZE = 300;
-
-const props = defineProps<ImageBlockProps>();
-
-const loading = ref<boolean>(true);
-const imageUrl = ref<string>('');
-const loadedDimensions = ref<NaturalDimensions>({ naturalWidth: 0, naturalHeight: 0 });
-const errorMessage = ref<string>('');
-
 const isSmallImage = computed(() => {
 	const { naturalWidth, naturalHeight } = loadedDimensions.value;
 	return naturalWidth < SMALL_IMAGE_SIZE && naturalHeight < SMALL_IMAGE_SIZE;
 });
-
-const setImage = (value: string) => {
-	imageUrl.value = value;
-	loading.value = false;
-};
 
 const setDimensions = (event: Event) => {
 	loadedDimensions.value = event.target as HTMLImageElement;
@@ -40,94 +28,27 @@ const setDimensions = (event: Event) => {
 	// already has a value from a prior upload.
 	triggerRef(loadedDimensions);
 };
-
-const handleError = (error: Error) => {
-	setImage('');
-	errorMessage.value = error.message;
-};
-
-const { loadMedia } = useMediaLoader(setImage, handleError);
-watchEffect(() => {
-	loadedDimensions.value = { naturalWidth: 0, naturalHeight: 0 };
-	errorMessage.value = '';
-
-	if (props.blobUrl != null) {
-		setImage(props.blobUrl);
-		return;
-	}
-
-	void loadMedia(props.resourceUrl);
-});
 </script>
 
 <template>
-	<div
-		:class="{
-			'image-block': true,
-			'broken-image': errorMessage?.length,
-			'small-image': !loading && isSmallImage,
-		}"
-	>
-		<!-- TODO: translations -->
+	<MediaBlockBase v-slot="{ mediaUrl, reportError }" :alt="alt" broken-file-image="broken-image.svg" :resource-url="resourceUrl" :blob-url="blobUrl" :is-small-media="isSmallImage">
 		<img
-			v-if="!loading && !errorMessage?.length"
-			:src="imageUrl"
+			:src="mediaUrl"
 			:alt="alt"
+			class="image-block"
 			@load="setDimensions"
-			@error="handleError(new Error(`Failed to load image. File: ${props.resourceUrl?.href}`))"
+			@error="reportError(new Error(`Failed to load image. File: ${resourceUrl?.href}`))"
 		>
-
-		<div v-if="loading" class="skeleton-loading" />
-
-		<template v-if="errorMessage?.length">
-			<img src="../../../assets/images/broken-image.svg" :alt="alt">
-			<p class="image-error-message">
-				{{ errorMessage }}
-			</p>
-		</template>
-	</div>
+	</MediaBlockBase>
 </template>
 
 <style scoped lang="scss">
 .image-block {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-	position: relative;
-	width: 100%;
-	overflow: hidden;
-
-	img {
-		max-height: var(--odk-max-image-height);
-		max-width: 100%;
-		width: auto;
-		height: auto;
-		display: block;
-		object-fit: contain;
-	}
-
-	&.small-image {
-		max-width: var(--odk-image-container-size);
-	}
-
-	&.broken-image img {
-		max-width: 90%;
-		margin-top: 10px;
-	}
-
-	.image-error-message {
-		margin: 20px;
-		font-size: var(--odk-hint-font-size);
-		font-weight: 300;
-		word-break: break-word;
-		text-align: center;
-		color: var(--odk-muted-text-color);
-	}
-
-	.skeleton-loading {
-		min-width: 300px;
-		min-height: 300px;
-	}
+	max-height: var(--odk-max-image-height);
+	max-width: 100%;
+	width: auto;
+	height: auto;
+	display: block;
+	object-fit: contain;
 }
 </style>
