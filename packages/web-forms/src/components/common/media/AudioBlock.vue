a03bfc8175a2cc9998769af85ad84d6373964151
latin-panda <66472237+latin-panda@users.noreply.github.com>
2026-01-07 22:06:47 +0700

    Adds media base component

--- packages/web-forms/src/components/common/media/AudioBlock.vue
+++ packages/web-forms/src/components/common/media/AudioBlock.vue
@@ -1,90 +1,28 @@
 <script setup lang="ts">
-import { useMediaLoader } from '@/components/common/media/useMediaLoader.ts';
+import MediaBlockBase from '@/components/common/media/MediaBlockBase.vue';
 import type { JRResourceURL } from '@getodk/common/jr-resources/JRResourceURL.ts';
-import { ref, watchEffect } from 'vue';
 
-interface AudioBlockProps {
+defineProps<{
 	readonly resourceUrl?: JRResourceURL;
 	readonly alt: string;
-}
-
-const props = defineProps<AudioBlockProps>();
-
-const loading = ref<boolean>(true);
-const audioUrl = ref<string>('');
-const errorMessage = ref<string>('');
-
-const setAudio = (value: string) => {
-	audioUrl.value = value;
-	loading.value = false;
-};
-
-const handleError = (error: Error) => {
-	loading.value = false;
-	audioUrl.value = '';
-	errorMessage.value = error.message;
-};
-
-const { loadMedia } = useMediaLoader(setAudio, handleError);
-watchEffect(() => {
-	errorMessage.value = '';
-	void loadMedia(props.resourceUrl);
-});
+}>();
 </script>
 
 <template>
-	<div :class="{ 'audio-block': true, 'broken-audio': errorMessage?.length }">
-		<!-- TODO: translations -->
+	<MediaBlockBase v-slot="{ mediaUrl, reportError }" :alt="alt" broken-file-image="broken-audio.svg" :resource-url="resourceUrl" :is-small-media="true">
 		<audio
-			v-if="!loading && !errorMessage?.length"
 			controls
-			:src="audioUrl"
+			:src="mediaUrl"
 			:title="alt"
-			@error="handleError(new Error(`Failed to load audio. File: ${props.resourceUrl?.href}`))"
+			class="audio-block"
+			@error="reportError(new Error(`Failed to load audio. File: ${resourceUrl?.href}`))"
 		/>
-
-		<div v-if="loading" class="skeleton-loading" />
-
-		<template v-if="errorMessage?.length">
-			<p class="audio-error-message">
-				{{ errorMessage }}
-			</p>
-		</template>
-	</div>
+	</MediaBlockBase>
 </template>
 
 <style scoped lang="scss">
 .audio-block {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-	position: relative;
-	width: 100%;
-	overflow: hidden;
-
-	audio {
-		max-width: 100%;
-		display: block;
-	}
-
-	&.broken-audio img {
-		max-width: 90%;
-		margin-top: 10px;
-	}
-
-	.audio-error-message {
-		margin: 20px;
-		font-size: var(--odk-hint-font-size);
-		font-weight: 300;
-		word-break: break-word;
-		text-align: center;
-		color: var(--odk-muted-text-color);
-	}
-
-	.skeleton-loading {
-		min-width: 300px;
-		min-height: 50px; // Adjusted for audio player height
-	}
+	max-width: 100%;
+	display: block;
 }
 </style>
