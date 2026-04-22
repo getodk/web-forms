da84392ee4e08e4ea474012e6cfbf7c7b53fa13b
latin-panda <66472237+latin-panda@users.noreply.github.com>
2026-04-23 01:53:34 +0700

    Fixes constant

--- packages/common/src/constants/datetime.ts
+++ packages/common/src/constants/datetime.ts
@@ -6,6 +6,8 @@ export const MILLISECOND_NANOSECONDS = BigInt(1_000_000);
 
 const ISO_DATE_LIKE_SUBPATTERN = '\\d{4}-\\d{2}-\\d{2}';
 
+export const ISO_DATE_LIKE_PATTERN = new RegExp(`^${ISO_DATE_LIKE_SUBPATTERN}(?=T|$)`);
+
 const STRICT_TIME_FORMATS = ['\\d{2}:\\d{2}:\\d{2}\\.\\d+', '\\d{2}:\\d{2}:\\d{2}'];
 
 // Strict: requires HH:MM:SS (with optional fractional seconds).
